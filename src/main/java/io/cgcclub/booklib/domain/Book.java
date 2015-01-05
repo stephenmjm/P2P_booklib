38b410abccc87c0cb6b7f00d66a6c2d95dfd8262
Calvin <calvin.xiao@ericsson.com>
2015-01-05 17:00:53 +0800

    init restful endpoint

--- src/main/java/io/cgcclub/booklib/domain/Book.java
+++ src/main/java/io/cgcclub/booklib/domain/Book.java
@@ -2,4 +2,7 @@ package io.cgcclub.booklib.domain;
 
 public class Book {
 
+	public String name;
+	public String url;
+	public Long ownerId;
 }
--- src/main/java/io/cgcclub/booklib/web/BookEndpoint.java
+++ src/main/java/io/cgcclub/booklib/web/BookEndpoint.java
@@ -1,13 +1,25 @@
 package io.cgcclub.booklib.web;
 
+import io.cgcclub.booklib.domain.Book;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
 
 @RestController
 public class BookEndpoint {
 
-	@RequestMapping(value = "/rest/books")
-	public String listAll() {
-		return "Wellcome to CGC Book Library";
+	public static final String JSON_UTF_8 = "application/json; charset=UTF-8";
+
+	@RequestMapping(value = "/rest/books", produces = JSON_UTF_8)
+	public List<Book> listAll() {
+		Book book = new Book();
+		book.name = "Big Data";
+		book.url = "http://book.douban.com/subject/25984046/";
+		book.ownerId = 1L;
+		return Lists.newArrayList(book);
 	}
 }
